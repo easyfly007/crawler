65974c0074847fd2aa43a16cb0a7b0c5d822919e
Yifei Huang <yfhuang@internal.synopsys.com>
2016-10-02 15:27:23 +0800

    for html_parser

--- baike_spider/html_parser.py
+++ baike_spider/html_parser.py
@@ -1,5 +1,42 @@
+# coding:utf-8
+
+import re
+from bs4 import BeautifulSoup
 
 class HtmlParser(object):
 
-	def output_html(self):
-		pass
+	def _get_new_urls(self, page_url, soup):
+		new_full_urls = set()
+		links = soup.find_all('a', href = re.compile(r'/view/\d+\.html'))
+		for link in links:
+			new_url = link['href']
+			new_full_url = urllib.parse.urljoin(page_url, new_url)
+			new_full_urls.add(new_full_url)
+		return new_full_urls
+
+
+	def _get_new_urls(self, page_url, soup):
+		
+		res_data = {}
+
+		res_data['url'] = page_url
+
+		#<dd class="lemmaWgt-lemmaTitle-title">
+		#	<h1>Python</h1>
+		#</dd>
+		title_node = soup.find('dd', class_ = 'lemmaWgt-lemmaTitle-title').find('h1')
+		res_data['title'] = title_node.get_text()
+
+		# <div class="lemma-summary" label-module="lemmaSummary">
+		summary_node = soup.find('div', class_ = 'lemma-summary')
+		res_data['summary'] = summary_node.get_ext()
+
+		return res_data
+
+	def output_html(self, page_url, html_cont):
+		if page_url is None or html_cont is None:
+			return
+		soup = BeautifulSoup(html_cont, 'html.parser', form_encoding = 'utf-8')
+		new_urls = self._get_new_urls(page_url, soup)
+		new_data = self._get_new_data(page_url, soup)
+		return new_urls,new_data	

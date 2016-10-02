15714f8e1612db2bf35b6189e503f2a48c2cf3f3
Yifei Huang <yfhuang@internal.synopsys.com>
2016-10-02 15:03:25 +0800

    html loader

--- baike_spider/html_downloader.py
+++ baike_spider/html_downloader.py
@@ -1,8 +1,13 @@
 # coding:utf-8
+import urllib
+
 
 class HtmlDownloader(object):
 	def download(self, url):
 		if url is None:
 			return None
-		urllib2
-		pass
+		response = urllib.request.urlopen(url)
+		if response.getcode() != 200:
+			return None
+
+		return response.read()
